378a9a2f9a2d540b387f22e3978eb90426d081f6
X <softthink02@gmail.com>
2026-07-02 15:08:41 +0100

    [react native course - project set up]

---
+++ src/app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text className='text-#741414'>Edit src/app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
