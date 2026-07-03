cdf5cd94c455be69687c479ed1c9b72796641b1f
X <softthink02@gmail.com>
2026-07-03 18:51:06 +0100

    [styling the tabs]

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,23 +1,86 @@
+import { ImageBackgroundHandler } from "@/components/tabsBgHandler";
+import Feather from "@expo/vector-icons/Feather";
 import { Tabs } from "expo-router";
 
 const _Layout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0f0D23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "#0f0D23",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
-        options={{ title: "Home", headerShown: false }}
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <ImageBackgroundHandler
+              focused={focused}
+              icon={<Feather name="home" size={20} color="#151312" />}
+              title="Home"
+            />
+          ),
+        }}
       />
       <Tabs.Screen
         name="search"
-        options={{ title: "Search", headerShown: false }}
+        options={{
+          title: "Search",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <ImageBackgroundHandler
+              focused={focused}
+              icon={<Feather name="search" size={20} color="#151312"  />}
+              title="Search"
+            />
+          ),
+        }}
       />
       <Tabs.Screen
         name="profile"
-        options={{ title: "Profile", headerShown: false }}
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <ImageBackgroundHandler
+              focused={focused}
+              icon={<Feather name="user" size={20} color="#151312"  />}
+              title="Profile"
+            />
+          ),
+        }}
       />
       <Tabs.Screen
         name="saved"
-        options={{ title: "Saved", headerShown: false }}
+        options={{
+          title: "Saved",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <ImageBackgroundHandler
+              focused={focused}
+              icon={<Feather name="bookmark" size={20} color="#151312"  />}
+              title="Saved"
+            />
+          ),
+        }}
       />
     </Tabs>
   );
---
+++ src/components/tabsBgHandler.tsx
@@ -0,0 +1,29 @@
+import { ImageBackground, Text, View } from "react-native";
+
+export const ImageBackgroundHandler = ({
+  focused,
+  icon,
+  title,
+}: {
+  focused: boolean;
+  icon: React.ReactNode;
+  title: string;
+}) => {
+  if (focused) {
+    return (
+      <ImageBackground
+        source={require("@/assets/images/purpleImage.png")}
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+      >
+        {icon}
+        <Text className="font-semibold text-base text-secondary ml-2">
+          {title}
+        </Text>
+      </ImageBackground>
+    );
+  } else {
+    <View className="size-full justify-center items-center mt-4 rounded-full">
+      {icon}
+    </View>;
+  }
+};
